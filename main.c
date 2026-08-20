447d05c44c278010c2c3a0a8d3e3baf29edbe96a
Adviti Pokalkar <318245105+Advitipklr04@users.noreply.github.com>
2026-08-20 19:58:59 +0530

    Add daily study goal

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    printf("Hello, GitHub!\n");
-    printf("This is my first C project!");
+    char name[50];
+    char subject[50];
+    int hours;
+    printf("Enter your name:");
+    scanf("%49s",name);
+    printf("Enter your subject:");
+    scanf("%49s",subject);
+    printf("Enter your daily study goal in hours:");
+    scanf("%d",&hours);
+    printf("Hello! %s\n",name);
+    printf("Your study subject is %s\n",subject);
+    printf("Your daily study goal is %d hours\n",hours);
     return 0;
 }
