836cc45be99c76601923b743c45864f8532f1bfe
Adviti Pokalkar <318245105+Advitipklr04@users.noreply.github.com>
2026-08-20 23:52:06 +0530

    Add multiple subject tracking

--- main.c
+++ main.c
@@ -2,16 +2,26 @@
 int main()
 {
     char name[50];
-    char subject[50];
+    char subjects[10][50];
     int hours;
+    int numberofsubjects;
     printf("Enter your name:");
     scanf("%49s",name);
-    printf("Enter your subject:");
-    scanf("%49s",subject);
+    printf("How many subjects do you have?");
+    scanf("%d",&numberofsubjects);
+    for(int i=0;i<numberofsubjects;i++)
+    {
+        printf("Enter the subject %d:",i+1);
+        scanf("%49s",subjects[i]);
+    }
     printf("Enter your daily study goal in hours:");
     scanf("%d",&hours);
     printf("Hello! %s\n",name);
-    printf("Your study subject is %s\n",subject);
+    printf("\nYour subjects are:\n");
+    for(int i=0;i<numberofsubjects;i++)
+    {
+        printf("%d. %s\n",i+1,subjects[i]);
+    }
     printf("Your daily study goal is %d hours\n",hours);
     return 0;
 }
