73f6663dd9c77298b2c1a753a315e1b64d6451e2
Adviti Pokalkar <318245105+Advitipklr04@users.noreply.github.com>
2026-08-21 00:24:23 +0530

    Add subject completion tracking

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main()
     char subjects[10][50];
     int hours;
     int numberofsubjects;
+    int status[10];
+    int choice;
     printf("Enter your name:");
     scanf("%49s",name);
     printf("How many subjects do you have?");
@@ -13,14 +15,25 @@ int main()
     {
         printf("Enter the subject %d:",i+1);
         scanf("%49s",subjects[i]);
+        status[i]=0;
     }
+    printf("Which subject did you complete?");
+    scanf("%d",&choice);
+    status[choice-1]=1;
     printf("Enter your daily study goal in hours:");
     scanf("%d",&hours);
     printf("Hello! %s\n",name);
     printf("\nYour subjects are:\n");
     for(int i=0;i<numberofsubjects;i++)
     {
-        printf("%d. %s\n",i+1,subjects[i]);
+        if(status[i]==1)
+        {
+            printf("%d. %s - Completed\n",i+1,subjects[i]);
+        }
+        else
+        {
+            printf("%d. %s - Pending\n",i+1,subjects[i]);
+        }
     }
     printf("Your daily study goal is %d hours\n",hours);
     return 0;
